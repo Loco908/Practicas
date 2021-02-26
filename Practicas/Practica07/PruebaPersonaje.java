3aa11b75b63271471b5aa029be39ecd8685bffdd
Loco908 <78123331+Loco908@users.noreply.github.com>
2021-02-26 09:57:45 -0600

    Ya terminé
    :)

--- Practicas/Practica07/PruebaPersonaje.java
+++ Practicas/Practica07/PruebaPersonaje.java
@@ -16,13 +16,32 @@ public class PruebaPersonaje {
       if(p1.setEdad(ed)){
         System.out.println(p1.getDetalle()+"Se modifico la edad");
       }else{
-        System.out.println(p1.getDetalle()+"Sin modificacion en edad");
+        System.out.println(p1.getDetalle()+"Sin modificacion en edad"+ed);
       }
 
       ed = (int)(Math.random()*250);
       if(p2.setEdad(ed)){
         System.out.println(p2.getDetalle()+"se modifico la edad");
+      }else{
+        System.out.println(p2.getDetalle()+"Sin modificacion la edad"+ed);
       }
+
+      ed = (int)(Math.random()*250);
+      if(p3.setEdad(ed)){
+        System.out.println(p3.getDetalle()+"Se modifico la edad");
+      }else{
+        System.out.println(p3.getDetalle()+"Sin modificacion la edad a"+ed);
+      }
+
+      do{
+        ed = (int)(Math.random()*250);
+        System.out.println(ed);
+        if(ed==150){
+          System.out.println("*****Bingo 150*****");
+          continue outfor;
+        }
+      }while(!p4.setEdad(ed));
+      System.out.println(p4.getDetalle()+"\n");
     }
   }  
 } 

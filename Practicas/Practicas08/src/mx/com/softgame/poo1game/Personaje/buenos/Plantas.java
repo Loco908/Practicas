f8476c964b80b33c8d2958a60ef10485a36df0fb
Loco908 <78123331+Loco908@users.noreply.github.com>
2021-03-05 09:56:11 -0600

    Vamos con los zombis

--- Practicas/Practicas08/src/mx/com/softgame/poo1game/Personaje/buenos/Plantas.java
+++ Practicas/Practicas08/src/mx/com/softgame/poo1game/Personaje/buenos/Plantas.java
@@ -1,16 +1,37 @@
 package mx.com.softgame.poo1game.Personaje.buenos;
-import mx.com.softgame.poo1game.personajes.Personaje;
+import mx.com.softgame.poo1game.Personaje.Personaje;
 
 public class Plantas extends Personaje {
     char escudo;
-}
-public char getEscudo() {
-    return escudo;
-}
-public Plantas(String nombre, int vida, char escudo2){
-    super(nombre,vida);
-    this.escudo = escudo2;
-}
-public Plantas(String nombre, char escudo){
-    this(nombre,3,escudo);
+
+    public char getEscudo() {
+        return escudo;
+    }
+    public Plantas(String nombre, int vida, char escudo) {
+        super(nombre,vida);
+        this.escudo = escudo;
+    }
+    public Plantas(String nombre, char escudo) {
+        this(nombre,3,escudo);
+    }
+    public Plantas(String nombre,int vida) {
+        this(nombre,vida,'A');
+    }
+    public String getDetalle() {
+        return super.getDetalle() + " " +escudo;
+    }
+    public void decVida() {
+        if(escudo == 'A'){
+            super.decVida(2);
+        }else{
+            super.decVida();
+        }
+    }
+    public void decVida(int restaVida) {
+        if(escudo == 'A'){
+            super.decVida(restaVida * 2);
+        }else{
+            super.decVida(restaVida);
+        }
+    }
 }

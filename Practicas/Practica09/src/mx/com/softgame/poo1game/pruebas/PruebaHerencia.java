50c0105275920dd8f050de22eb771b9a7b1712c3
Loco908 <78123331+Loco908@users.noreply.github.com>
2021-03-12 10:41:12 -0600

    Modificando
    subida 9

--- Practicas/Practica09/src/mx/com/softgame/poo1game/pruebas/PruebaHerencia.java
+++ Practicas/Practica09/src/mx/com/softgame/poo1game/pruebas/PruebaHerencia.java
@@ -3,18 +3,18 @@ import java.lang.Math;
 import mx.com.softgame.poo1game.personajes.Personaje;
 import mx.com.softgame.poo1game.personajes.buenos.Planta;
 import mx.com.softgame.poo1game.personajes.malos.Zombie;
+
 public class PruebaHerencia{
     public static void main(String[]args){
-        Personaje per01 = new Personaje("Valeria",99);
-        Personaje per02 = new Personaje("Octavio");
-        Planta plan01 = new Planta("Wendy",10,'B');
-        Planta plan02 = new Planta("Ricardo",50);
-        Planta plan03 = new Planta("vania",'C');
-        Planta plan04 = new Planta("Alan");
-        Zombie zom01 = new Zombie("Karen",80,true);
-        Zombie zom02 = new Zombie("Eduardo",true);
-        Zombie zom03 = new Zombie("Esmeralda");
-        Personaje [] personajes = {per01,per02,plan03,plan04,zom01,zom02,zom03};  
+        Personaje [] personajes = {new Personaje("Omar",99),
+        new Planta("Karen",50),
+        new Zombie("Hanna",80,false),
+
+        new Personaje("Omar",99),
+        new Planta("Karen",50),
+        new Zombie("Hanna",80,false)
+    };
+  
         for (Personaje j: personajes){
             System.out.println(j.getDetalle());
             if(j instanceof Planta){

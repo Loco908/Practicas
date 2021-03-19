5eaf4e57d9e6d36c1a9e11536f9b3e59ab4bd9c1
Loco908 <78123331+Loco908@users.noreply.github.com>
2021-03-19 08:34:56 -0600

    Cambie el método de public a private

--- Practicas/Practica10/src/mx/com/softgame/poo1game/utlis/Utileria.java
+++ Practicas/Practica10/src/mx/com/softgame/poo1game/utlis/Utileria.java
@@ -2,7 +2,7 @@ package mx.com.softgame.poo1game.utlis;
 
 public class Utileria {
     int contadorID;
-    public static int getId(int contadorId) {
+    private static int getId(int contadorId) {
         int contadorID;
         contadorID(++contadorId);
 

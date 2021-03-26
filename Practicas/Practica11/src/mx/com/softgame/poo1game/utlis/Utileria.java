269a8bbe855cb832edf464ff36d9049dedb54bad
Luis <78123331+Loco908@users.noreply.github.com>
2021-03-26 07:52:46 -0600

    Add files via upload

---
+++ Practicas/Practica11/src/mx/com/softgame/poo1game/utlis/Utileria.java
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+package mx.com.softgame.poo1game.utlis;
+
+public class Utileria {
+    private static int contadorID;// Para que nadie pueda entra al metodo del contador
+    public static int getID() {
+        return ++contadorID;
+    }
+
+}
+=======
+package mx.com.softgame.poo1game.utlis;
+
+public class Utileria {
+    private static int contadorID;// Para que nadie pueda entra al metodo del contador
+    public static int getID() {
+        return ++contadorID;
+    }
+
+}
+>>>>>>> db98810647bcb186e4f4a99900ac7505a5ab1419
